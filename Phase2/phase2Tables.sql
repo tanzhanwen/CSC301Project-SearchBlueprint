19e98695b354f5bba39a4502c36617f17ca65613
Zhanwen Tan <tanzhanwen@gmail.com>
2015-03-05 15:17:10 -0500

    The schema definition and the modified reporting python file

---
+++ Phase2/phase2Tables.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE DomainName
+(
+domain_id BIGSERIAL PRIMARY KEY,
+domain_name char(30) NOT NULL
+);
+
+CREATE TABLE Address
+(
+address_id BIGSERIAL PRIMARY KEY,
+address_name char(100) NOT NULL,
+domain_id int references DomainName(domain_id)
+);
