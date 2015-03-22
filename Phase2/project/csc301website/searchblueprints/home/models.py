f9624dd1ee6dfdb0a5f982ff89ff24835fdca55e
lifejuggler <jtchoiboy@gmail.com>
2015-03-22 17:52:35 -0400

    added model definitions
    added models to generate database

--- Phase2/project/csc301website/searchblueprints/home/models.py
+++ Phase2/project/csc301website/searchblueprints/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Users(models.Model):
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+class Glossary(models.Model):
+    term = models.CharField(max_length=30)
+    definition = models.TextField()
+class DomainName(models.Model):
+    domain_name = models.CharField(max_length=30)
+class Address(models.Model):
+    address_name = models.CharField(max_length=30)
+    domain = models.ForeignKey(DomainName)
+
 # Create your models here.
