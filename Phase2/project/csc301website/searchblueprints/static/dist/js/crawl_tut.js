163dee6572fe7e71a72811b66e1ed04a41484a1c
vrprojects <vignesh.ravi@mail.utoronto.ca>
2015-03-28 13:51:49 -0400

    Made a js function for quizzes
    Refactored quiz fade out and in function. The new function can be used
    in any of the pages with any number of arguments for correct and
    incorrect answers

--- Phase2/project/csc301website/searchblueprints/static/dist/js/crawl_tut.js
+++ Phase2/project/csc301website/searchblueprints/static/dist/js/crawl_tut.js
@@ -3,7 +3,7 @@ var pageNumber = 0;
 		function navPage(increment)
 		{
 		    var d  = 0;
-		     if(increment && pageNumber<3)
+		     if(increment && pageNumber<10)
 		    {
 		        pageNumber = pageNumber +1; 
 				d = 1;
@@ -20,34 +20,62 @@ var pageNumber = 0;
 				$('#container' + pageNumber).show();
 			 if(pageNumber ==1){
 			 cr_l1();
-				$('.btn').attr("disabled","disabled");
+				//$('.btn').attr("disabled","disabled");
 			 
 
+			 }
+			 if(pageNumber ==2){
+			 cr_l2();
+			 
+			 }
+			 if(pageNumber ==3){
+			 cr_l3();
+			 
 			 }
 			
 			}
 		    }
+			
+		function fadeAnswer(txt,txt2){
+			var all_fade = txt.split(",");
+		
+			for (var i = 0; i < all_fade.length; i++) {
+				$( all_fade[i]).fadeOut( 400 );
+   
+			}
+			
+			$( txt2).delay( 400 ).fadeIn( 400 );
+			//$('.btn').removeAttr('disabled');
+		
+		}
 		function cr_l1(){
 			
 			$('.hid').hide();
 			 
 			$( "#cr1_1" ).delay( 1000 ).fadeIn( 400 );
 			$( "#cr1_2" ).delay( 2000 ).fadeIn( 400 );
-			$( "#cr1_3" ).delay( 5000 ).fadeIn( 400 );
-			$( "#cr1_4" ).delay( 5000 ).fadeIn( 400 );
-			$( "#cr1_5" ).delay( 5000 ).fadeIn( 400 );
+			$( "#cr1_3" ).delay( 3000 ).fadeIn( 400 );
+			$( "#cr1_4" ).delay( 3500 ).fadeIn( 400 );
+			$( "#cr1_5" ).delay( 4000 ).fadeIn( 400 );
 		}
-		function cr1(){
-			$( "#cr1_5" ).fadeOut( 400 );
-			$( "#cr1_6" ).delay( 400 ).fadeIn( 400 );
-			$('.btn').removeAttr('disabled');
-		
-		
-		}
-		function cr2(){
-			$( "#cr1_5" ).fadeOut( 400 );
-			$( "#cr1_7" ).delay( 400 ).fadeIn( 400 );
-			$('.btn').removeAttr('disabled');
+
+		function cr_l2(){
 			
+			$('.hid2').hide();
+			 
+			$( "#cr2_1" ).show();
+			$( "#cr2_2" ).delay( 1000 ).fadeIn( 300 );
+			$( "#cr2_3" ).delay( 2000 ).fadeIn( 300 );
+			$( "#cr2_4" ).delay( 3000 ).fadeIn( 300 );
+
+		}
+		function cr_l3(){
+			
+			$('.hid3').hide();
+			 
+			$( "#cr3_1" ).delay( 1000 ).fadeIn( 300 );
+			$( "#cr3_2" ).delay( 2000 ).fadeIn( 300 );
+			$( "#cr3_3" ).delay( 3000 ).fadeIn( 300 );
+
 		}
 		
