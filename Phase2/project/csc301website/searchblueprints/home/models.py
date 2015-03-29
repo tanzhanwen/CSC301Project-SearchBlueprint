3abf9a9781083cbd2ca9fc7a72a5682bb218fde6
lifejuggler <jtchoiboy@gmail.com>
2015-03-29 16:41:04 -0400

    added more columns for the database

--- Phase2/project/csc301website/searchblueprints/home/models.py
+++ Phase2/project/csc301website/searchblueprints/home/models.py
@@ -4,12 +4,22 @@ class Users(models.Model):
     login = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
 class Glossary(models.Model):
-    term = models.CharField(max_length=30)
+    term = models.CharField(unique = True)
     definition = models.TextField()
 class DomainName(models.Model):
-    domain_name = models.CharField(max_length=30)
+	id = models.AutoField(primary_key=True)
+    domain_name = models.TextField(unique = True)
 class Address(models.Model):
-    address_name = models.CharField(max_length=30)
-    domain = models.ForeignKey(DomainName)
+	id = models.AutoField(primary_key=True)
+	name = models.TextField
+    url = models.TextField(unique = True)
+    domain = models.ForeignKey
+    error = models.IntegerField
+    old_rank = models.IntegerField
+    new_rank = models.IntegerField
+    key_word = models.TextField
+    description = models.TextField
+    score = models.IntegerField
+
 
 # Create your models here.
