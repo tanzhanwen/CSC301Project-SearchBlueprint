c4b98ac9997d39ff79a7692f972edd976b6107dd
lifejuggler <jtchoiboy@gmail.com>
2015-03-29 16:37:57 -0400

    Revert "added more columns"
    This reverts commit afff56a435a9f5559b03e4bddf2b47b37a6010ae.

--- Phase2/project/csc301website/searchblueprints/home/models.py
+++ Phase2/project/csc301website/searchblueprints/home/models.py
@@ -4,22 +4,12 @@ class Users(models.Model):
     login = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
 class Glossary(models.Model):
-    term = models.CharField(unique = True)
+    term = models.CharField(max_length=30)
     definition = models.TextField()
 class DomainName(models.Model):
-	id = models.AutoField(primary_key=True)
-    domain_name = models.TextField(unique = True)
+    domain_name = models.CharField(max_length=30)
 class Address(models.Model):
-	id = models.AutoField(primary_key=True)
-	name = models.TextField
-    url = models.TextField(unique = True)
-    domain = models.ForeignKey
-    error = models.IntegerField
-    old_rank = models.IntegerField
-    new_rank = models.IntegerField
-    key_word = models.TextField
-    description = models.TextField
-    score = models.IntegerField
-
+    address_name = models.CharField(max_length=30)
+    domain = models.ForeignKey(DomainName)
 
 # Create your models here.
